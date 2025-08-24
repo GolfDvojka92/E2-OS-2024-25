ce4cdc3ed7fcaaad838e9e4afaf7a9941feebc15
Aljosa <aljosaspika@gmail.com>
2025-08-24 15:40:46 +0200

    Uradio sesti

--- vezba02/Zadaci/06_stl_sort_opadajuce/main.cpp
+++ vezba02/Zadaci/06_stl_sort_opadajuce/main.cpp
@@ -1,13 +1,28 @@
 /*
-Uneti 5 celih brojeva sa standardnog ulaza.
-Korišćenjem STL algoritma sortirati brojeve u opadajućem redosledu.
+Uneti 5 celih brojela sa standardnog ulaza.
+Korišćenjem STL algoritma sortirati brojele u opadajućem redosledu.
 */
 
 #include <iostream>
+#include <list>
+#include <string>
 
 using namespace std;
 
 int main()
 {
+    list<int> l;
+    string unos;
+    cout << "Unos (<C-d> za kraj): ";
+    while (getline(cin,unos)) {
+        l.push_back(stoi(unos));
+    }
+    l.sort();
+    l.reverse();
+    cout << "["; 
+    for (auto it = l.begin(); it != prev(l.end(), 1); it++) {
+        cout << *it << ", ";
+    }
+    cout << *prev(l.end(), 1) << "]" << endl;
     return 0;
 }
