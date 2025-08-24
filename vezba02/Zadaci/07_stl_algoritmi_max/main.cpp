dd2b50368d901e230e36373733dc09c73f2f58c7
Aljosa <aljosaspika@gmail.com>
2025-08-24 15:50:23 +0200

    Gotov sedmi

--- vezba02/Zadaci/07_stl_algoritmi_max/main.cpp
+++ vezba02/Zadaci/07_stl_algoritmi_max/main.cpp
@@ -3,11 +3,21 @@ Korisnik treba da unese 5 celih brojeva koristeći tastaturu (standardni ulaz).
 Korišćenjem STL algoritma, među tih 5 brojeva potrebno je pronaći najveći.
 */
 
+#include <algorithm>
 #include <iostream>
+#include <string>
+#include <vector>
 
 using namespace std;
 
 int main()
 {
+    vector<int> v;
+    string unos;
+    cout << "Unos (<C-d> za kraj): ";
+    while (getline(cin,unos)) {
+        v.push_back(stoi(unos));
+    }
+    cout << "Najveci je: " << *max_element(v.begin(),v.end()) << endl;
     return 0;
 }
