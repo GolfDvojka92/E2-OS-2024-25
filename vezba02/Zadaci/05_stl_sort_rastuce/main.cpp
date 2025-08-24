9962aa89ecacf873dcf9c653585102417595992d
Aljosa <aljosaspika@gmail.com>
2025-08-24 15:36:41 +0200

    Uradio peti

--- vezba02/Zadaci/05_stl_sort_rastuce/main.cpp
+++ vezba02/Zadaci/05_stl_sort_rastuce/main.cpp
@@ -4,10 +4,24 @@ Korišćenjem STL algoritma sortirati brojeve u rastućem redosledu.
 */
 
 #include <iostream>
+#include <string>
+#include <list>
 
 using namespace std;
 
 int main()
 {
+    list<int> v;
+    string unos;
+    cout << "Unos (<C-d> za kraj): ";
+    while (getline(cin,unos)) {
+        v.push_back(stoi(unos));
+    }
+    v.sort();
+    cout << "["; 
+    for (auto it = v.begin(); it != prev(v.end(), 1); it++) {
+        cout << *it << ", ";
+    }
+    cout << *prev(v.end(), 1) << "]" << endl;
     return 0;
 }
