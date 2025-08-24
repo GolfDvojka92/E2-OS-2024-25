cf5ecc0deab5b03e208e8f9189cbda99ccee4815
Aljosa <aljosaspika@gmail.com>
2025-08-24 13:42:33 +0200

    Gotov drugi

--- vezba02/Zadaci/02_stl_iterator/main.cpp
+++ vezba02/Zadaci/02_stl_iterator/main.cpp
@@ -3,10 +3,24 @@ Napraviti program koji prihvata proizvoljan broj linija sa standardnog ulaza i z
 */
 
 #include <iostream>
+#include <list>
 
 using namespace std;
 
 int main()
 {
+    list<string> l;
+    string unos;
+    cout << "Unos (<C-d> za kraj): ";
+    while (getline(cin,unos)) {
+        l.push_back(unos);
+    }
+    cout << "Uneseni stringovi su: " << endl;
+    /*for (const string &s : l) {
+        cout << s << endl;
+    }*/
+    for (auto it = l.begin(); it != l.end(); it++) {
+        cout << *it << endl;
+    }
     return 0;
 }
